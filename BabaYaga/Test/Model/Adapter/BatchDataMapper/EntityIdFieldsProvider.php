9c9fad43f78c0b701b5730a63b74a4f049d56ca2
noonlit <noonlit@users.noreply.github.com>
2020-09-01 18:52:27 +0300

    Update EntityIdFieldsProvider.php

--- BabaYaga/Test/Model/Adapter/BatchDataMapper/EntityIdFieldsProvider.php
+++ BabaYaga/Test/Model/Adapter/BatchDataMapper/EntityIdFieldsProvider.php
@@ -6,9 +6,6 @@ namespace BabaYaga\Test\Model\Adapter\BatchDataMapper;
 
 use Magento\AdvancedSearch\Model\Adapter\DataMapper\AdditionalFieldsProviderInterface;
 
-/**
- * Provide data mapping for price fields
- */
 class EntityIdFieldsProvider implements AdditionalFieldsProviderInterface
 {
     /**
